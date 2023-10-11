8d9b68f516cdbb84566b0d46e1d8f4b5522bb622
Nana Adjei Manu <claeusdev@gmail.com>
2023-10-11 15:40:27 +0200

    fix broken type

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,7 @@ function IconTabs() {
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    console.log(event);
     setValue(newValue);
   };
 
